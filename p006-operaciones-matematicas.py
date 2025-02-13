d6271e2cd899559d8ead7d005bb1cf0144ed32df
GUES851213 <abrahham.13@gmail.com>
2025-02-13 10:36:35 -0600

    Actividad 3

---
+++ p006-operaciones-matematicas.py
@@ -0,0 +1,17 @@
+# Ejemplifica Operaciones matematicas basicas
+
+import os;os.system('clear')
+
+
+x = 10.5
+y = 2.5
+
+suma = x+y
+resta = x-y
+mult = x*y
+div = x/y
+res = x % y
+exp = x ** y
+dive = x // y
+print(f'suma {suma}\nresta {resta}\nmultiplicacion {mult}\ndivision {div}')
+print(f'residuo {res}\nexponenciacion {exp}\ndivision entera {dive}')
